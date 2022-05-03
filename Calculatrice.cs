0c12d82d70b013b7c54bd8a55b45cc3bd372d7ea
Ahmed Toumi <ahmed.toumi2002@gmail.com>
2022-05-03 11:14:50 -0400

    Initial commit TDD

---
+++ Calculatrice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public class Calculatrice
+    {
+
+        public int Add(string input = "")
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return 0;
+
+
+            //if(input.Contains())
+            string[] delimeter = new string[] { ";/" }; 
+            string[] chainesNombres = input.Split(delimeter, StringSplitOptions.None);
+            int[] nombres = chainesNombres.Select(int.Parse).ToArray();
+
+
+            int somme = 0;
+            foreach (int nb in nombres)
+            {
+                somme += nb;
+            }
+
+
+
+            return somme;
+        }
+    }
+}
---
+++ FizzBuzz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public class FizzBuzz
+    {
+        public string ConvertNumber(int input)
+        {
+            string response = "";
+            bool estDivisibleParTois = EstDivisiblePar(input, 3);
+            bool estDivisibleParCinq= EstDivisiblePar(input, 5);
+
+            if (estDivisibleParTois)
+            {
+                response += "fizz";
+            }
+
+            if (estDivisibleParCinq)
+            {
+                response += "buzz";
+            }
+
+            bool isNormalNumber = !estDivisibleParTois && !estDivisibleParCinq;
+            if (isNormalNumber)
+            {
+                response = input.ToString();
+            }
+
+            return response;
+        }
+
+        public bool EstDivisiblePar(int number, int divider)
+        {
+            if (divider == 0)
+                return false;
+
+            return (number % divider == 0);
+
+        }
+    }
+}
